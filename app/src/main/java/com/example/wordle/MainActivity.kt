42f4666c921edc2f6e41d080a9e187d21fd6fe96
mugamta <tjwlgns02@gmail.com>
2022-05-09 17:55:11 +0900

    20220509
    한 것
    1. 엔터 키를 누르면 단어가 삭제되지만 개행 문자가 남는 현상을 개선했다.
      -> 이는 activity_main.xml의 EditText에 singleLine 속성을 추가하여 해결했다.
    2. 입력된 문자는 자동으로 영문 대문자로 변환되게 하였다.
    3. 6글자인 경우에만 지워지게 하였다.
    
    수정해야 할 것
    1. WORDLE 알고리즘을 6개의 when으로 표기했는데, 이를 개선하여 작성 가능할지 확인한다.
    2. 입력된 문자를 영문 대문자로 변환하는 것이 아니라, 입력 칸에서 대문자만 입력 가능하게 해본다.
    
    다음 할 것
    1. 기본 WORDLE 게임처럼 6번의 시도 기회를 주고, 성공/실패를 나누어보자.

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -1,43 +1,102 @@
 package com.example.wordle
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.KeyEvent.KEYCODE_ENTER
 import android.widget.EditText
 import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    var flag = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var inputWord = findViewById<EditText>(R.id.InputWord)
+        val inputWord = findViewById<EditText>(R.id.InputWord)
+        var answer : String = "WORDLE"
 
-        var word1_1 = findViewById<TextView>(R.id.word1_1)
-        var word1_2 = findViewById<TextView>(R.id.word1_2)
-        var word1_3 = findViewById<TextView>(R.id.word1_3)
-        var word1_4 = findViewById<TextView>(R.id.word1_4)
-        var word1_5 = findViewById<TextView>(R.id.word1_5)
-        var word1_6 = findViewById<TextView>(R.id.word1_6)
+        val word1_1 = findViewById<TextView>(R.id.word1_1)
+        val word1_2 = findViewById<TextView>(R.id.word1_2)
+        val word1_3 = findViewById<TextView>(R.id.word1_3)
+        val word1_4 = findViewById<TextView>(R.id.word1_4)
+        val word1_5 = findViewById<TextView>(R.id.word1_5)
+        val word1_6 = findViewById<TextView>(R.id.word1_6)
 
         inputWord.setOnKeyListener{ v, keyCode, event ->
             if (event.action == KeyEvent.ACTION_DOWN && keyCode == KEYCODE_ENTER){
                 if(inputWord.text.length == 6){
-                    var str = inputWord.text.toString()
-                    word1_1.text = str[0].toString()
-                    word1_2.text = str[1].toString()
-                    word1_3.text = str[2].toString()
-                    word1_4.text = str[3].toString()
-                    word1_5.text = str[4].toString()
-                    word1_6.text = str[5].toString()
+                    val str = inputWord.text.toString().uppercase(Locale.getDefault()) //대문자 변환
+
+                    val strList : MutableList<Char> = str.toMutableList()
+
+                    word1_1.text = str[0].toString(); word1_2.text = str[1].toString()
+                    word1_3.text = str[2].toString(); word1_4.text = str[3].toString()
+                    word1_5.text = str[4].toString(); word1_6.text = str[5].toString()
+
+                    word1_1.setTextColor(Color.WHITE); word1_2.setTextColor(Color.WHITE)
+                    word1_3.setTextColor(Color.WHITE); word1_4.setTextColor(Color.WHITE)
+                    word1_5.setTextColor(Color.WHITE); word1_6.setTextColor(Color.WHITE)
+
+                    when {
+                        answer[0] == strList[0] -> { word1_1.setTextColor(Color.GREEN)}
+                        answer[0] == strList[1] -> { word1_2.setTextColor(Color.YELLOW)}
+                        answer[0] == strList[2] -> { word1_3.setTextColor(Color.YELLOW)}
+                        answer[0] == strList[3] -> { word1_4.setTextColor(Color.YELLOW)}
+                        answer[0] == strList[4] -> { word1_5.setTextColor(Color.YELLOW)}
+                        answer[0] == strList[5] -> { word1_6.setTextColor(Color.YELLOW)}
+                    }
+
+                    when {
+                        answer[1] == strList[0] -> { word1_1.setTextColor(Color.YELLOW)}
+                        answer[1] == strList[1] -> { word1_2.setTextColor(Color.GREEN)}
+                        answer[1] == strList[2] -> { word1_3.setTextColor(Color.YELLOW)}
+                        answer[1] == strList[3] -> { word1_4.setTextColor(Color.YELLOW)}
+                        answer[1] == strList[4] -> { word1_5.setTextColor(Color.YELLOW)}
+                        answer[1] == strList[5] -> { word1_6.setTextColor(Color.YELLOW)}
+                    }
+
+                    when {
+                        answer[2] == strList[0] -> { word1_1.setTextColor(Color.YELLOW)}
+                        answer[2] == strList[1] -> { word1_2.setTextColor(Color.YELLOW)}
+                        answer[2] == strList[2] -> { word1_3.setTextColor(Color.GREEN)}
+                        answer[2] == strList[3] -> { word1_4.setTextColor(Color.YELLOW)}
+                        answer[2] == strList[4] -> { word1_5.setTextColor(Color.YELLOW)}
+                        answer[2] == strList[5] -> { word1_6.setTextColor(Color.YELLOW)}
+                    }
+
+                    when {
+                        answer[3] == strList[0] -> { word1_1.setTextColor(Color.YELLOW)}
+                        answer[3] == strList[1] -> { word1_2.setTextColor(Color.YELLOW)}
+                        answer[3] == strList[2] -> { word1_3.setTextColor(Color.YELLOW)}
+                        answer[3] == strList[3] -> { word1_4.setTextColor(Color.GREEN)}
+                        answer[3] == strList[4] -> { word1_5.setTextColor(Color.YELLOW)}
+                        answer[3] == strList[5] -> { word1_6.setTextColor(Color.YELLOW)}
+                    }
+
+                    when {
+                        answer[4] == strList[0] -> { word1_1.setTextColor(Color.YELLOW)}
+                        answer[4] == strList[1] -> { word1_2.setTextColor(Color.YELLOW)}
+                        answer[4] == strList[2] -> { word1_3.setTextColor(Color.YELLOW)}
+                        answer[4] == strList[3] -> { word1_4.setTextColor(Color.YELLOW)}
+                        answer[4] == strList[4] -> { word1_5.setTextColor(Color.GREEN)}
+                        answer[4] == strList[5] -> { word1_6.setTextColor(Color.YELLOW)}
+                    }
+
+                    when {
+                        answer[5] == strList[0] -> { word1_1.setTextColor(Color.YELLOW)}
+                        answer[5] == strList[1] -> { word1_2.setTextColor(Color.YELLOW)}
+                        answer[5] == strList[2] -> { word1_3.setTextColor(Color.YELLOW)}
+                        answer[5] == strList[3] -> { word1_4.setTextColor(Color.YELLOW)}
+                        answer[5] == strList[4] -> { word1_5.setTextColor(Color.YELLOW)}
+                        answer[5] == strList[5] -> { word1_6.setTextColor(Color.GREEN)}
+                    }
+
+                    inputWord.setText("")
                 }
-                inputWord.setText("")
             }
             false
         }
-
-
     }
 }
