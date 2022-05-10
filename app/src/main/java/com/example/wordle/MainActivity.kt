217ee24ae8a3b7a0d9d7e4b98521602cd1afead4
Jihun Seo <tjwlgns02@gmail.com>
2022-05-10 23:22:36 +0900

    20220510
    한 것
    1. WORDLE 정답 확인 알고리즘을 개선했다.
    2. 영문 대문자만 입력 가능하게 변경하였다.
    3. 단어의 길이가 6이 아니여도 작동할 수 있게 하였다.
    4. 6번의 시도 기회를 주고, 순서대로 입력을 시도한다.
    5. 성공하거나 시도 기회를 모두 소진 시 메세지가 출력된다.
    
    다음 할 것
    1. 좌측 바에서 설정 창을 만들고, 단어의 길이를 선택할 수 있게 하자.
    2. 시도 횟수를 가변적으로 설정하고, 최대 10번의 시도가 가능하게 하자.

--- app/src/main/java/com/example/wordle/MainActivity.kt
+++ app/src/main/java/com/example/wordle/MainActivity.kt
@@ -3,100 +3,104 @@ package com.example.wordle
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputFilter
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
 import android.view.KeyEvent
 import android.view.KeyEvent.KEYCODE_ENTER
+import android.view.View
 import android.widget.EditText
 import android.widget.TextView
-import java.util.*
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnKeyListener {
+    var answer = "WORDLE"
+    var answerLength = answer.length
+
+    lateinit var word1 : TextView; lateinit var word2 : TextView; lateinit var word3 : TextView
+    lateinit var word4 : TextView; lateinit var word5 : TextView; lateinit var word6 : TextView
+
+    lateinit var inputWord1 : EditText; lateinit var inputWord2 : EditText; lateinit var inputWord3 : EditText
+    lateinit var inputWord4 : EditText; lateinit var inputWord5 : EditText; lateinit var inputWord6 : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val inputWord = findViewById<EditText>(R.id.InputWord)
-        var answer : String = "WORDLE"
+        //filter를 통하여 입력이 정답의 길이인 answer_length를 넘기지 못하게 조정한다.
+        inputWord1 = findViewById(R.id.Input1); inputWord1.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(answerLength))
+        inputWord2 = findViewById(R.id.Input2); inputWord2.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(answerLength))
+        inputWord3 = findViewById(R.id.Input3); inputWord3.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(answerLength))
+        inputWord4 = findViewById(R.id.Input4); inputWord4.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(answerLength))
+        inputWord5 = findViewById(R.id.Input5); inputWord5.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(answerLength))
+        inputWord6 = findViewById(R.id.Input6); inputWord6.filters = arrayOf<InputFilter>(InputFilter.LengthFilter(answerLength))
 
-        val word1_1 = findViewById<TextView>(R.id.word1_1)
-        val word1_2 = findViewById<TextView>(R.id.word1_2)
-        val word1_3 = findViewById<TextView>(R.id.word1_3)
-        val word1_4 = findViewById<TextView>(R.id.word1_4)
-        val word1_5 = findViewById<TextView>(R.id.word1_5)
-        val word1_6 = findViewById<TextView>(R.id.word1_6)
+        word1 = findViewById(R.id.word1); word2 = findViewById(R.id.word2); word3 = findViewById(R.id.word3)
+        word4 = findViewById(R.id.word4); word5 = findViewById(R.id.word5); word6 = findViewById(R.id.word6)
 
-        inputWord.setOnKeyListener{ v, keyCode, event ->
-            if (event.action == KeyEvent.ACTION_DOWN && keyCode == KEYCODE_ENTER){
-                if(inputWord.text.length == 6){
-                    val str = inputWord.text.toString().uppercase(Locale.getDefault()) //대문자 변환
+        //setOnKeyListener는 외부 함수에 구현하였다.
+        inputWord1.setOnKeyListener(this); inputWord2.setOnKeyListener(this); inputWord3.setOnKeyListener(this)
+        inputWord4.setOnKeyListener(this); inputWord5.setOnKeyListener(this); inputWord6.setOnKeyListener(this)
+    }
 
-                    val strList : MutableList<Char> = str.toMutableList()
+    override fun onKey(v: View?, keyCode: Int, event: KeyEvent?): Boolean { //View.OnKeyListener를 상속받는 함수
+        if (event?.action == KeyEvent.ACTION_DOWN && keyCode == KEYCODE_ENTER) { //Enter키 입력시 작동
+            when(v?.id){ //n번째 시도에 따라 분류
+                R.id.Input1 -> { setTextView(inputWord1, inputWord2, word1) }
+                R.id.Input2 -> { setTextView(inputWord2, inputWord3, word2) }
+                R.id.Input3 -> { setTextView(inputWord3, inputWord4, word3) }
+                R.id.Input4 -> { setTextView(inputWord4, inputWord5, word4) }
+                R.id.Input5 -> { setTextView(inputWord5, inputWord6, word5) }
+                R.id.Input6 -> { setTextView(inputWord6, inputWord6, word6) }
+            }
+        }
+        return false
+    }
 
-                    word1_1.text = str[0].toString(); word1_2.text = str[1].toString()
-                    word1_3.text = str[2].toString(); word1_4.text = str[3].toString()
-                    word1_5.text = str[4].toString(); word1_6.text = str[5].toString()
+    fun setTextView(edit1: EditText, edit2: EditText, tv: TextView){ //입력을 계산하여 textview에 대입하는 함수
+        if(edit1.text.length == answerLength){
+            val str = edit1.text.toString() //입력된 문자열
 
-                    word1_1.setTextColor(Color.WHITE); word1_2.setTextColor(Color.WHITE)
-                    word1_3.setTextColor(Color.WHITE); word1_4.setTextColor(Color.WHITE)
-                    word1_5.setTextColor(Color.WHITE); word1_6.setTextColor(Color.WHITE)
+            val spannableStringBuilder = correctAnswer(answer, str) //정답과 문자열 대조하여 색칠된 문자열 반환
 
-                    when {
-                        answer[0] == strList[0] -> { word1_1.setTextColor(Color.GREEN)}
-                        answer[0] == strList[1] -> { word1_2.setTextColor(Color.YELLOW)}
-                        answer[0] == strList[2] -> { word1_3.setTextColor(Color.YELLOW)}
-                        answer[0] == strList[3] -> { word1_4.setTextColor(Color.YELLOW)}
-                        answer[0] == strList[4] -> { word1_5.setTextColor(Color.YELLOW)}
-                        answer[0] == strList[5] -> { word1_6.setTextColor(Color.YELLOW)}
+            tv.text = spannableStringBuilder // 초록/노란색으로 칠해진 결과를 TextView대입
+
+            edit1.setText("") //입력한 edittext는 textview로 전달되었으므로 비움
+
+            when { //정답일때는 메세지를 출력, edit1 == edit2일때는 시도가 모두 실패한 경우이므로 실패 메세지를 출력, 이외에는 다음 입력 활성화
+                str == answer -> { Toast.makeText(this.applicationContext, "단어 ${str}은 정답입니다!", Toast.LENGTH_SHORT).show() }
+                edit1 == edit2 -> { Toast.makeText(this.applicationContext, "실패하셨습니다..", Toast.LENGTH_SHORT).show() }
+                else -> edit2.isEnabled = true
+            }
+            edit1.isEnabled = false //입력되었으므로 사용 불가능하게 변경
+        }
+    }
+
+    fun correctAnswer(answer: String, str: String): SpannableStringBuilder { //입력과 정답을 비교하여 문자열을 색칠하는 함수
+        val spannableStringBuilder = SpannableStringBuilder(str)
+
+        for(i in 0 until answerLength){ //answer의 문자를 하나씩 순회하며 위치와 일치하면 녹색, 아니면 나머지 글자에서 존재하는지 탐색
+            if(answer[i] == str[i]){ //같은 경우 녹색으로 처리
+                spannableStringBuilder.setSpan(
+                    ForegroundColorSpan(Color.GREEN),
+                    i,
+                    i+1,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)  //색, 시작 구간, 끝 구간, 삽입된 구간을 제외하고 이를 삽입
+            }
+            else{
+                for(j in 0 until answerLength){
+                    if(i != j && answer[i] == str[j]){ //다른 경우 노란색으로 처리
+                        spannableStringBuilder.setSpan(
+                            ForegroundColorSpan(Color.YELLOW),
+                            j,
+                            j+1,
+                            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                        break //같은 글자가 여러개 있을 수 있으므로 현재 answer[i]에 대해 찾아냈으면 종료
                     }
-
-                    when {
-                        answer[1] == strList[0] -> { word1_1.setTextColor(Color.YELLOW)}
-                        answer[1] == strList[1] -> { word1_2.setTextColor(Color.GREEN)}
-                        answer[1] == strList[2] -> { word1_3.setTextColor(Color.YELLOW)}
-                        answer[1] == strList[3] -> { word1_4.setTextColor(Color.YELLOW)}
-                        answer[1] == strList[4] -> { word1_5.setTextColor(Color.YELLOW)}
-                        answer[1] == strList[5] -> { word1_6.setTextColor(Color.YELLOW)}
-                    }
-
-                    when {
-                        answer[2] == strList[0] -> { word1_1.setTextColor(Color.YELLOW)}
-                        answer[2] == strList[1] -> { word1_2.setTextColor(Color.YELLOW)}
-                        answer[2] == strList[2] -> { word1_3.setTextColor(Color.GREEN)}
-                        answer[2] == strList[3] -> { word1_4.setTextColor(Color.YELLOW)}
-                        answer[2] == strList[4] -> { word1_5.setTextColor(Color.YELLOW)}
-                        answer[2] == strList[5] -> { word1_6.setTextColor(Color.YELLOW)}
-                    }
-
-                    when {
-                        answer[3] == strList[0] -> { word1_1.setTextColor(Color.YELLOW)}
-                        answer[3] == strList[1] -> { word1_2.setTextColor(Color.YELLOW)}
-                        answer[3] == strList[2] -> { word1_3.setTextColor(Color.YELLOW)}
-                        answer[3] == strList[3] -> { word1_4.setTextColor(Color.GREEN)}
-                        answer[3] == strList[4] -> { word1_5.setTextColor(Color.YELLOW)}
-                        answer[3] == strList[5] -> { word1_6.setTextColor(Color.YELLOW)}
-                    }
-
-                    when {
-                        answer[4] == strList[0] -> { word1_1.setTextColor(Color.YELLOW)}
-                        answer[4] == strList[1] -> { word1_2.setTextColor(Color.YELLOW)}
-                        answer[4] == strList[2] -> { word1_3.setTextColor(Color.YELLOW)}
-                        answer[4] == strList[3] -> { word1_4.setTextColor(Color.YELLOW)}
-                        answer[4] == strList[4] -> { word1_5.setTextColor(Color.GREEN)}
-                        answer[4] == strList[5] -> { word1_6.setTextColor(Color.YELLOW)}
-                    }
-
-                    when {
-                        answer[5] == strList[0] -> { word1_1.setTextColor(Color.YELLOW)}
-                        answer[5] == strList[1] -> { word1_2.setTextColor(Color.YELLOW)}
-                        answer[5] == strList[2] -> { word1_3.setTextColor(Color.YELLOW)}
-                        answer[5] == strList[3] -> { word1_4.setTextColor(Color.YELLOW)}
-                        answer[5] == strList[4] -> { word1_5.setTextColor(Color.YELLOW)}
-                        answer[5] == strList[5] -> { word1_6.setTextColor(Color.GREEN)}
-                    }
-
-                    inputWord.setText("")
                 }
             }
-            false
         }
+        return spannableStringBuilder
     }
 }
